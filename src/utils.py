eec78c8e59af4187187656dd8fc005efdaabd0f7
tuan.nh <tuan.nh@tinasoft.com.vn>
2026-03-25 16:00:57 +0700

    first commit

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ def visualize_tsne(visualize_classes, saved_features, mode="photo"):
     label_to_color = {
         "cow": "#E2514A",
         "raccoon": "#F5AA53",
-        "scissors": "#F3D97B",
+        "scissors": "#FCE283",
         "seagull": "#EAF890",
         "sword": "#8ACA8F",
         "tree": "#4DA3B5",
